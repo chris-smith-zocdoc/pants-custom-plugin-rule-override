1f56d74bb65d74b4239646c325d1810a7cd8f58c
chris.smith <chris.smith@zocdoc.com>
2025-06-26 23:06:41 -0400

    canonical_name replacement + rule filter. works on 2.27 and 2.28

--- pants-plugins/plugin1/register.py
+++ pants-plugins/plugin1/register.py
@@ -6,47 +6,32 @@ from pants.engine.unions import UnionRule
 import dataclasses
 from pants.backend.experimental.javascript import register as js_register
 
-from .moneypatches import setup_node_tool_process, rules as moneypatches_rules
-
-# This monkeypatch method works in both 2.27 and 2.28!!
-from pants.backend.javascript.subsystems import nodejs as pants_nodejs
-# Need to monkeypatch here
-pants_nodejs.setup_node_tool_process = setup_node_tool_process
-
-def single_or_raise(rules: Iterable[TaskRule], lambda_filter: Callable[[TaskRule], bool]) -> TaskRule:
-    matching_rules = [
-        r for r in rules 
-        if lambda_filter(r)
-    ]
-    if len(matching_rules) != 1:
-        raise Exception(f"Expected 1 matching rule, got {len(matching_rules)}\n{matching_rules}")
-    return matching_rules[0]
-
+from .moneypatches import rules as moneypatches_rules
 
 def rules() -> Iterable[Union[Rule, UnionRule]]:
     
-    custom_rules = [
-        *moneypatches_rules(),
-    ]
+    def ignore_rule(rule):
+        return isinstance(rule, TaskRule) and rule.canonical_name in [
+            "pants.backend.javascript.subsystems.nodejs.setup_node_tool_process",
+        ]
+    
     original_rules = [
-        *js_register.rules(),
+        r for r in js_register.rules()
+        # Remove the original rule
+        if not ignore_rule(r)
     ]
     
-    custom_rule = single_or_raise(
-        custom_rules,
-        lambda r: isinstance(r, TaskRule) and r.func.__name__ == "setup_node_tool_process"
-    )
-
-    patched_rules = []
-    for rule in original_rules:
+    custom_rules = []
+    for rule in moneypatches_rules():
         if isinstance(rule, TaskRule) and rule.func.__name__ == "setup_node_tool_process":
-            # And also replace the callable stored in the original rule
-            patched_rules.append(dataclasses.replace(rule, func=custom_rule.func))
+            # Set the canonical name of our moneypatch to the original rule
+            custom_rules.append(dataclasses.replace(rule, canonical_name="pants.backend.javascript.subsystems.nodejs.setup_node_tool_process"))
         else:
-            patched_rules.append(rule)
+            custom_rules.append(rule)
 
     return [
-        *patched_rules,
+        *original_rules,
+        *custom_rules,
     ]
 
 
